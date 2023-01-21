fe4621a4d66c29ab8201bedd8c03083f97953c44
Andreas Granig <andreas@granig.com>
2023-01-21 22:03:31 +0100

    Fetch proxyconfig from server on startup
    We need this to distinguish between different environments, and
    we don't know this on build time yet

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+fetch('proxyconfig.json').then(res => res.json()).then((resp) => {
+    console.log(res);
+    console.log(resp);
+    platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}).catch(err => console.error(err));
